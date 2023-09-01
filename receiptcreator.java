d14f1753699bb08717ee45fb01bfa621e85df302
zhouand25 <zhouand25@7hills.org>
2023-09-01 12:43:59 -0400

    Currently Broken

--- receiptcreator.java
+++ receiptcreator.java
@@ -5,10 +5,27 @@ public class receiptcreator {
         for(int i=0;i<40;++i) {
             output+="*";
         }
-        System.out.println("How many Golden Apples would you like?");
-        Scanner quant = new Scanner(System.in);
-        int qApples = quant.nextInt();
-        System.out.println("I'm Sorry we don't have gold nor apples. But, today we have a special deal! I can sell you "+qApples+" Watermelons?");
-        System.out.print("I'm just kidding we don't sell that either :). This is a bank.");
+        System.out.println("How many Red Apples would you like? 1$ per apple");
+        Scanner quantRed = new Scanner(System.in);
+        int qRed = quantRed.nextInt();
+        System.out.println("How many Iron Apples would you like? 50$ per apple");
+        Scanner quantIron = new Scanner(System.in);
+        int qIron = quantIron.nextInt();
+        System.out.println("How many Golden Apples would you like? 10000$ per apple, Trust me friend this is a bargain :)");
+        Scanner quantGold = new Scanner(System.in);
+        int qGold = quantGold.nextInt();
+
+        int total=(1*qRed)+(50*qIron)+(10000*qGold);
+        double fulltotal=total*1.03;
+        double tax=total*0.03;
+
+        output="\n"+"Your Purchase:\n"+qRed+" Red Apples"+"\n"+qIron+" Iron Apples"+"\n"+qGold+" Gold Apples"+"\n";
+        output="Price of Goods: "+total+".00$\n";
+        output="Tax: "+tax+"$ Trust me friend pay your taxes, it's not worth messing with the IRS!\n";
+        output="Final: "+fulltotal+"$\n";
+        for(int i=0;i<40;++i) {
+            output+="*";
+        }
+        System.out.print(output);
     }
 }
