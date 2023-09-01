4324b1d3695efb3a8a60477e0257f99e6eb74413
zhouand25 <zhouand25@7hills.org>
2023-09-01 12:29:28 -0400

    Test again

--- receiptcreator.java
+++ receiptcreator.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
 public class receiptcreator {
     public static void main(String[] args) {
         String output="";
-        System.out.print("World");
+        for(int i=0;i<40;++i) {
+            output+="*";
+        }
+        System.out.println("How many Golden Apples would you like?");
+        Scanner quant = new Scanner(System.in);
+        int qApples = quant.nextInt();
+        System.out.println("I'm Sorry we don't have gold nor apples. But, today we have a special deal! I can sell you "+qApples+" Watermelons?");
+        System.out.print("I'm just kidding we don't sell that either :). This is a bank.");
     }
 }
